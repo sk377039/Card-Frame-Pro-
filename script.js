fd3bc12550d05b5437c73f1a16328c639043b56e
sk377039 <sk3770395@gmail.com>
2026-08-05 20:36:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button");
 
+  button.addEventListener("click", function () {
+    alert("Upload feature is coming in the next step!");
+  });
+});
